aa7d1ee5863c984706c2061483995c437b044319
Pankaj Kumar <104948046+PankajKumardev@users.noreply.github.com>
2024-08-01 21:17:03 +0530

    Update Users.jsx

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -36,14 +36,14 @@ function User({user}) {
 
     return <div className="flex justify-between">
         <div className="flex">
-            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
-                <div className="flex flex-col justify-center h-full text-xl">
+            <div className="rounded-full h-12 w-12 bg-slate-700 flex justify-center mt-1 mr-2">
+                <div className="flex flex-col justify-center h-full text-xl text-blue-200 ">
                     {user.firstname[0]}
                 </div>
             </div>
             <div className="flex flex-col justify-center h-ful">
                 <div>
-                    {user.firstName} {user.lastname}
+                    {user.firstname} {user.lastname}
                 </div>
             </div>
         </div>
