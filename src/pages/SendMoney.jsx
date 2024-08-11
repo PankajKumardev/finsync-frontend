7d3a9e249f6c4a710db78f76f97658cafdbf497e
Pankaj Kumar <104948046+PankajKumardev@users.noreply.github.com>
2024-08-11 11:51:19 +0530

    Update SendMoney.jsx

--- src/pages/SendMoney.jsx
+++ src/pages/SendMoney.jsx
@@ -49,6 +49,7 @@ export const SendMoney = () => {
                                             }
                                         });
                                         navigate('/dashboard'); 
+                                         window.alert(`Transfer sucessfully to ${name} of amount Rs.${amount}`)
                                     }}
                                     className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white"
                                 >
